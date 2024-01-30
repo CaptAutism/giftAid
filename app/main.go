d75c45328b726030c7d23414bde097feab331d06
George Venn <gvenn@npex42.dev>
2024-01-30 17:55:07 +0000

    dep(echo): Added Echo

--- app/main.go
+++ app/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	fmt.Printf("Hello, World\n")
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World\n")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
